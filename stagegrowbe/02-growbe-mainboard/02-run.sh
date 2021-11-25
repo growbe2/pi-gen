a20c9314c39c8a6ca70bd2bed76d708fad41f7fe
William Quintal <william95quintalwilliam@outlook.com>
2021-11-25 17:37:31 -0500

    pi-gen

--- stagegrowbe/02-growbe-mainboard/02-run.sh
+++ stagegrowbe/02-growbe-mainboard/02-run.sh
@@ -1,7 +1,11 @@
 
-install -m 644 files/growbe-mainboard@.service "${ROOTFS_DIR}/usr/lib/systemd/system"
-
 mkdir -p "${ROOTFS_DIR}/opt/growbe/"
 
+install -m 644 files/growbe-mainboard@.service "${ROOTFS_DIR}/usr/lib/systemd/system"
+install -m 644 files/init-config.service "${ROOTFS_DIR}/usr/lib/systemd/system"
+install -m 644 files/init.sh "${ROOTFS_DIR}/opt/growbe/init.sh"
+
 on_chroot << EOF
+chmod +x /opt/growbe/init.sh
+systemctl enabled init-config.service
 EOF
