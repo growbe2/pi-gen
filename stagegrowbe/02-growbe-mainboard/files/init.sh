7a6d8027c3bb3b903c9c5929be58ce950093339f
William Quintal <william95quintalwilliam@outlook.com>
2021-11-25 17:53:23 -0500

    update init script.

--- stagegrowbe/02-growbe-mainboard/files/init.sh
+++ stagegrowbe/02-growbe-mainboard/files/init.sh
@@ -1,3 +1,10 @@
 # ! /bin/bash
 
-cp -r /boot/growbe/* /opt/growbe
+if [ -n "$(ls -A /boot/growbe/ 2>/dev/null)" ]
+then
+	cp -r /boot/growbe/* /opt/growbe
+	/opt/growbe/configure.sh
+	rm -rf /boot/growbe/*
+else
+  echo "empty (or does not exist)"
+fi
