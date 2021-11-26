37fe2ec9dd41406a1419eaef84907c6bb73ae14e
William Quintal <william95quintalwilliam@outlook.com>
2021-11-25 23:30:11 -0500

    wip

--- stagegrowbe/02-growbe-mainboard/00-run.sh
+++ stagegrowbe/02-growbe-mainboard/00-run.sh
@@ -4,10 +4,13 @@ mkdir -p "${ROOTFS_DIR}/opt/growbe/"
 install -m 644 files/growbe-mainboard@.service "${ROOTFS_DIR}/etc/systemd/system"
 install -m 644 files/init-config.service "${ROOTFS_DIR}/etc/systemd/system"
 install -m 644 files/init.sh "${ROOTFS_DIR}/opt/growbe/init.sh"
+chmod +x "${ROOTFS_DIR}/opt/growbe/init.sh"
 
 on_chroot << EOF
-chmod +x /opt/growbe/init.sh
+echo 1
 systemctl enabled init-config.service
+echo 2
 echo "i2c-dev" >> /etc/modules
+echo 3
 sed -i '1s/^/dtparam=i2c_arm=on\n/' /boot/config.txt
 EOF
